48a71c484a721d2e496a8dc14324e5f94644606f
Ulysses B <wagren@kth.se>
2022-05-05 20:26:01 +0200

    Refactor tensor and function

--- autograd/autograd/function.py
+++ autograd/autograd/function.py
@@ -6,35 +6,28 @@ class Function(object):
     def __init__(self, *tensors):
         self.parents = tensors
         self.saved_tensors = []
-        self.requires_grad = any([t.requires_grad for t in tensors if
-            isinstance(t, Tensor)])
+        self.requires_grad = _tensors_require_grad(tensors)
 
     def save_for_backward(self, *tensors):
         self.saved_tensors.extend(tensors)
 
-    def apply(self, op, *inputs, **kwargs):
-        ctx = op(self, *inputs)
-        res = Tensor(ctx.forward(self.data,
-            *[t.data for t in inputs], **kwargs))
-        res._ctx = ctx
-        return res
+    def apply(self, func, *inputs, **kwargs):
+        # by registering the ops to the Tensor as an attribute, `self` in
+        # this case is the Tensor calling the op, `func` is the 
+        # uninitialized operation to perform, e.g. Add, Sub, Mean, Dot etc.
+        # `*tensors` are the Tensors to use together with `self` in the `func`,
+        # **kwargs are used to specify op specific behavior
+        context = func(self, *tensors)
+        result = Tensor(context.forward(self.data,
+            *[t.data for t in tensors], **kwargs),
+            requires_grad=context.requires_grad)
 
+        result._ctx = context
+        return result
 
-class Add(Function):
-    def forward(self, x, y):
-        return x + y
-    
-    def backward(self, prev_grad):
-        return prev_grad, prev_grad
-setattr(Tensor, 'add', partialmethod(Add.apply, Add))
+def _tensors_require_grad(*tensors):
+    return any([t.requires_grad for t in tensors if isinstance(t, Tensor)])
 
-class Sum(Function):
-    def forward(self, x, axis=None, keepdims=True):
-        self.save_for_backward(x.shape)
-        return x.sum(axis=axis, keepdims=keepdims)
-
-    def backward(self, prev_grad):
-        xshape, = self.saved_tensors
-        return prev_grad * np.ones(xshape)
-setattr(Tensor, 'sum', partialmethod(Sum.apply, Sum))
+def _register(name, func):
+    setattr(Tensor, name, partialmethod(func.apply, func))
 
--- autograd/autograd/tensor.py
+++ autograd/autograd/tensor.py
@@ -1,29 +1,6 @@
 import numpy as np
 
 class Tensor(object):
-    """ Wrapping numpy.ndarray for forward/backward pass operations.
-    Initialize object with either of (int, float, list, tuple, np.ndarray),
-    specify if the Tensor is going to require a gradient leaf to be memorized
-    during forward pass for the backward pass, determine dtype of Tensor.
-
-    Example usage
-    -------------
-    Create some tensors.
-        >>> tensor1 = Tensor(np.ones((5,1,4)), dtype=np.float64)
-        >>> tensor2 = Tensor([[-4.0, -1, 15.2]], requires_grad=True)
-
-    Init parameters
-    ---------------
-    data: int | float | list | tuple | np.ndarray
-        The data modality to wrap as a Tensor, if the provided data is not
-        valid, the __init__ func raises a ValueError and exits.
-    requires_grad: bool
-        True/False whether or not to require gradient in backwards pass.
-    dtype: int | float | np.dtype
-        The data type object to cast the input data as. It will subsequentially
-        be the dtype of the initialized Tensor object.
-
-    """
     def __init__(self, data, requires_grad=False, dtype=np.float32):
         self.requires_grad = requires_grad
 
@@ -80,46 +57,22 @@ class Tensor(object):
         return self.data.dtype
         
     def backward(self, allow_fill=True):
-        """ Recursively perform gradient backwards pass for
-        all parents with requires_grad=True from this current Tensor. 
-        Call ends when either at user created Tensors, i.e., no 
-        function context tied to the Tensor, or, the Tensor does not
-        require grad.
-
-        Example usage
-        -------------
-        Invoke the backwards pass on Tensor generated from operation.
-            >>> x = Tensor(np.ones((128, 784)))
-            >>> W = Tensor(np.ones((784, 64)), requires_grad=True)
-            >>> y = x.dot(W).mean()
-            >>> y.backward()
-
-        Parameters
-        ----------
-        allow_fill: bool
-            Should be False for all Tensors that are no the one
-            invoking the initial backwards call. For the first
-            gradient, all values are set to 1, since
-            grad_X/grad_X == 1, so fill grad with ones.
-
-        """
-        if not self._ctx:
+        if self._ctx is None:
             return
-        
+
         if self.grad is None and allow_fill:
-            assert self.shape == (1, )
-            self.grad = np.ones_like(self.data).reshape((-1, 1))
+            self.grad = np.ones(self.shape)
 
         parents = self._ctx.parents
-        grads = self._ctx.backward(self.grad)
-        grads = [grads] if len(parents) == 1 else grads
+        gradients = self._ctx.backward(self.grad)
+        if isinstance(gradients, np.ndarray):
+            gradients = [gradients]
 
-        for grad, parent in zip(grads, parents):
-            if grad is None:
+        for gradient, parent in zip(gradients, parents):
+            if gradient is None:
                 continue
 
             if parent.requires_grad:
-                parent.grad = grad
-
-            parent.backward(allow_fill=False)
+                parent.grad = gradient
+                parent.backward(allow_fill=False)
 
