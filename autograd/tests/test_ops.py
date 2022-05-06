c761b3e2a681533f3417b0c808958223e7f42aec
Ulysses B <wagren@kth.se>
2022-05-06 17:21:07 +0200

    more tests, passing (#2)

--- autograd/tests/test_ops.py
+++ autograd/tests/test_ops.py
@@ -5,7 +5,7 @@ import unittest
 from autograd import Tensor
 from functools import partial
 
-def _test_op(shapes, torch_func, autograd_func, name, timeits=2):
+def _test_op(shapes, torch_func, autograd_func, name, timeits=10):
     torch_t = [torch.tensor(np.random.random(size=shape), requires_grad=True)
             for shape in shapes]
     autograd_t = [Tensor(t.detach().numpy(), requires_grad=True) for t in torch_t]
@@ -44,6 +44,32 @@ class TestOps(unittest.TestCase):
     def test_sub(self):
         _test_op([(100, 100), (100, 100)], lambda x, y: x - y, Tensor.sub, 'sub')
 
+    def test_matmul(self):
+        _test_op([(128, 784), (784, 64)], lambda x, y: x.matmul(y), Tensor.matmul, 'matmul')
+
     def test_mean(self):
         _test_op([(78, 152)], lambda t: t.mean(), Tensor.mean, 'mean')
+        _test_op([(20, 15, 2)], lambda t: t.mean(dim=1, keepdims=True),
+                lambda t: Tensor.mean(t, axis=1, keepdims=True), 'mean-args')
+        _test_op([(100, 15, 32, 72)], lambda t: t.mean(dim=(2, 3), keepdims=True),
+                lambda t: Tensor.mean(t, axis=(2, 3), keepdims=True), 'mean-args')
+        _test_op([(30, 40, 50)], lambda t: t.mean(dim=(0, 1, 2), keepdims=False),
+                lambda t: Tensor.mean(t, axis=(0, 1, 2), keepdims=False), 'mean-args')
+
+    def test_sum(self):
+        _test_op([(40, 784)], lambda t: t.sum(), Tensor.sum, 'sum')
+        _test_op([(64, 1, 1, 8)], lambda t: t.sum(axis=1, keepdims=True),
+                lambda t: Tensor.sum(t, axis=1, keepdims=True), 'sum-args')
+        _test_op([(14, 51, 7)], lambda t: t.sum(axis=(1, 2), keepdims=True),
+                lambda t: Tensor.sum(t, axis=(1, 2), keepdims=True), 'sum-args')
+        _test_op([(5, 10, 8, 1)], lambda t: t.sum(axis=1, keepdims=True),
+                lambda t: Tensor.sum(t, axis=1, keepdims=True), 'sum-args')
+        _test_op([(2, 4, 1)], lambda t: t.sum(axis=(0, 1), keepdims=False),
+                lambda t: Tensor.sum(t, axis=(0, 1), keepdims=False), 'sum-args')
+
+    def test_exp(self):
+        _test_op([(120, 156, 80)], lambda t: t.exp(), Tensor.exp, 'exp')
+
+    def test_log(self):
+        _test_op([(154, 78, 2, 201)], lambda t: t.log(), Tensor.log, 'log')
 
