bd9380bb6a3947dd2278d277f8f8e65df53a95c9
Ulysses B <wagren@kth.se>
2022-05-08 15:48:10 +0200

    LSTM implementation, needs to fix Tensor to be subscriptable, (#10)

--- leaf/nn.py
+++ leaf/nn.py
@@ -26,8 +26,14 @@ class Dense(Module):
         return result
 
 class LSTM(Module):
-    def __init__(self, input_size, hidden_size, n_layers):
-        pass
+    def __init__(self, input_size, hidden_size, n_layers=1):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        self.W_ih = Tensor.uniform(input_size, hidden_size*4, requires_grad=True)
+        self.W_hh = Tensor.uniform(hidden_size, hidden_size*4, requires_grad=True)
+        self.b_ih = Tensor.uniform(hidden_size*4, requires_grad=True)
+        self.b_hh = Tensor.uniform(hidden_size*4, requires_grad=True)
 
     def forward(self, x, initial_states=None):
         batch_size, seq_len, item_len = x.shape
@@ -42,8 +48,17 @@ class LSTM(Module):
 
         for t in range(seq_len):
             x_t = x[:, t, :]
-            gates = x_t @ self.W_ih.T + h_t @ self.W_hh.T + self.b_ih + self.b_hh
+            gates = x_t.matmul(self.W_ih.T).add(h_t.matmul(self.W_hh.T)).add(self.b_ih.add(self.b_hh))
 
-            i_t, f_t, g_t, o_t = 0,0,0,0
+            i_t, f_t, g_t, o_t = gates.chunk(chunks=4, dim=1)
+            i_t = i_t.sigmoid()
+            f_t = f_t.sigmoid()
+            g_t = g_t.tanh()
+            o_t = o_t.sigmoid()
+            c_t = f_t.multiply(c_t).add(i_t.multiply(g_t))
+            h_t = o_t.multiply(c_t.tanh())
+            hidden_seq.append(h_t)
+        print(hidden_seq)
+
 
 
