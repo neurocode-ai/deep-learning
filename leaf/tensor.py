ced8bf672932088f00df0eceac4090557d1a673e
Ulysses B <wagren@kth.se>
2022-05-08 14:32:23 +0200

    changes to backward and init, note what tensors are leafs for autograd

--- leaf/tensor.py
+++ leaf/tensor.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Tensor(object):
-    def __init__(self, data, requires_grad=False, dtype=np.float32):
+    def __init__(self, data, requires_grad=False, dtype=np.float32, _idx=None, _isleaf=True):
         self.requires_grad = requires_grad
 
         if isinstance(data, int) or isinstance(data, float):
@@ -19,10 +19,11 @@ class Tensor(object):
         self.data = data
         self.grad = None
         self._ctx = None
+        self._idx = _idx
+        self._isleaf = _isleaf
 
     def __str__(self):
-        return f'<leaf.Tensor(\n{self.data}\n'
-        + f'dtype={self.dtype}, grad_fn={self._ctx}, grad={self.grad}>'
+        return f'<leaf.Tensor(\n{self.data}\n'+f'dtype={self.dtype}, grad_fn={self._ctx}, grad={self.grad}>'
 
     @classmethod
     def zeros(cls, *shape, **kwargs):
@@ -55,8 +56,14 @@ class Tensor(object):
     @property
     def dtype(self):
         return self.data.dtype
-        
+
+    def chunk(self, chunks, dim=0):
+        chunked = np.split(self.data, chunks, dim)
+
     def backward(self, allow_fill=True):
+        if allow_fill:
+            self.grad = None
+
         if self._ctx is None:
             return
 
@@ -64,7 +71,7 @@ class Tensor(object):
             self.grad = np.ones(self.shape)
 
         parents = self._ctx.parents
-        gradients = self._ctx.backward(self.grad)
+        gradients = self._ctx.backward(self.grad, _idx=self._idx)
         if isinstance(gradients, np.ndarray):
             gradients = [gradients]
 
@@ -73,6 +80,13 @@ class Tensor(object):
                 continue
 
             if parent.requires_grad:
-                parent.grad = gradient
+                if parent.grad is None: 
+                    gradient.flags.writeable = True
+                    parent.grad = gradient
+                else:
+                    if not parent._isleaf:
+                        parent.grad = gradient
+                    else:
+                        parent.grad += gradient
                 parent.backward(allow_fill=False)
 
