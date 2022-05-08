e5520d453e274337a4988f17db33a0c95e184bec
Ulysses B <wagren@kth.se>
2022-05-08 14:31:43 +0200

    refactor **kwargs so _idx can be passed to chunk, implement chunk, (#7)

--- leaf/ops.py
+++ leaf/ops.py
@@ -10,7 +10,7 @@ class Add(Function):
         self.save_for_backward(x.shape, y.shape)
         return x + y
 
-    def backward(self, prev_grad):
+    def backward(self, prev_grad, **kwargs):
         xshape, yshape, = self.saved_tensors
         return np.broadcast_to(prev_grad, xshape), np.broadcast_to(prev_grad, yshape)
 _register('add', Add)
@@ -20,7 +20,7 @@ class Sub(Function):
         self.save_for_backward(x.shape, y.shape)
         return x - y
 
-    def backward(self, prev_grad):
+    def backward(self, prev_grad, **kwargs):
         xshape, yshape, = self.saved_tensors
         return np.broadcast_to(prev_grad, xshape), -np.broadcast_to(prev_grad, yshape)
 _register('sub', Sub)
@@ -30,7 +30,7 @@ class Matmul(Function):
         self.save_for_backward(x, y)
         return x.dot(y)
 
-    def backward(self, prev_grad):
+    def backward(self, prev_grad, **kwargs):
         x, y, = self.saved_tensors
         return prev_grad.dot(y.T), x.T.dot(prev_grad)
 _register('matmul', Matmul)
@@ -41,7 +41,7 @@ class Mean(Function):
         self.save_for_backward(x.shape, out.shape)
         return out * np.prod(out.shape) / np.prod(x.shape)
 
-    def backward(self, prev_grad):
+    def backward(self, prev_grad, **kwargs):
         xshape, outshape, = self.saved_tensors
         return np.ones(xshape) * prev_grad * np.prod(outshape) / np.prod(xshape)
 _register('mean', Mean)
@@ -52,7 +52,7 @@ class Exp(Function):
         self.save_for_backward(result)
         return result
     
-    def backward(self, prev_grad):
+    def backward(self, prev_grad, **kwargs):
         result, = self.saved_tensors
         return prev_grad * result
 _register('exp', Exp)
@@ -62,7 +62,7 @@ class Log(Function):
         self.save_for_backward(x)
         return np.log(x)
 
-    def backward(self, prev_grad):
+    def backward(self, prev_grad, **kwargs):
         x, = self.saved_tensors
         return prev_grad / x
 _register('log', Log)
@@ -72,7 +72,7 @@ class Sum(Function):
         self.save_for_backward(x.shape)
         return x.sum(axis=axis, keepdims=keepdims)
     
-    def backward(self, prev_grad):
+    def backward(self, prev_grad, **kwargs):
         xshape, = self.saved_tensors
         return np.ones(xshape) * prev_grad
 _register('sum', Sum)
@@ -83,7 +83,7 @@ class Sigmoid(Function):
         self.save_for_backward(result)
         return result
     
-    def backward(self, prev_grad):
+    def backward(self, prev_grad, **kwargs):
         result, = self.saved_tensors
         return prev_grad * result * (1 - result)
 _register('sigmoid', Sigmoid)
@@ -93,7 +93,7 @@ class ReLU(Function):
         self.save_for_backward(x)
         return np.maximum(x, 0)
 
-    def backward(self, prev_grad):
+    def backward(self, prev_grad, **kwargs):
         x, = self.saved_tensors
         return prev_grad * (x >= 0)
 _register('relu', ReLU)
@@ -105,7 +105,7 @@ class Tanh(Function):
         self.save_for_backward(result)
         return result
 
-    def backward(self, prev_grad):
+    def backward(self, prev_grad, **kwargs):
         result, = self.saved_tensors
         return prev_grad * (1 - np.power(result, 2))
 _register('tanh', Tanh)
@@ -115,7 +115,7 @@ class Pow(Function):
         self.save_for_backward(x, y)
         return np.power(x, y)
 
-    def backward(self, prev_grad):
+    def backward(self, prev_grad, **kwargs):
         x, y, = self.saved_tensors
         return (np.broadcast_to(prev_grad * y * np.power(x, y - 1), x.shape), None)
 _register('pow', Pow)
@@ -125,9 +125,21 @@ class Reshape(Function):
         self.save_for_backward(x.shape)
         return x.reshape(shape)
 
-    def backward(self, prev_grad):
+    def backward(self, prev_grad, **kwargs):
         xshape, = self.saved_tensors
         return prev_grad.reshape(xshape)
 _register('reshape', Reshape)
 
+class Chunk(Function):
+    def forward(self, x, chunks=1, dim=0):
+        result = np.split(x, chunks, dim)
+        self.save_for_backward(x.shape, chunks, dim)
+        return result
+
+    def backward(self, prev_grad, _idx=None):
+        xshape, chunks, dim, = self.saved_tensors
+        remake = [np.zeros_like(prev_grad) if i != _idx else prev_grad for i in range(chunks)]
+        result = np.concatenate(remake, axis=dim).reshape(xshape)
+        return result
+_register('chunk', Chunk)
 
