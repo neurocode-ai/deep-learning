f7762c4d5b76a29513dc874e33127694c022e88f
Ulysses B <wagren@kth.se>
2022-05-08 14:33:00 +0200

    started working on LSTM, WIP, (#10)

--- leaf/nn.py
+++ leaf/nn.py
@@ -12,7 +12,6 @@ class Module(object):
         # TODO: implement this
         return []
 
-
 class Dense(Module):
     def __init__(self, fan_in, fan_out, use_bias=True):
         self.weight_ = Tensor.uniform(fan_in, fan_out)
@@ -25,3 +24,26 @@ class Dense(Module):
             return result.add(self.bias_)
 
         return result
+
+class LSTM(Module):
+    def __init__(self, input_size, hidden_size, n_layers):
+        pass
+
+    def forward(self, x, initial_states=None):
+        batch_size, seq_len, item_len = x.shape
+        hidden_seq = []
+        HS = self.hidden_size
+
+        if initial_states is None:
+            h_t, c_t = (
+                    Tensor.zeros(batch_size, HS, requires_grad=True),
+                    Tensor.zeros(HS, HS, requires_grad=True))
+        else: h_t, c_t = initial_states
+
+        for t in range(seq_len):
+            x_t = x[:, t, :]
+            gates = x_t @ self.W_ih.T + h_t @ self.W_hh.T + self.b_ih + self.b_hh
+
+            i_t, f_t, g_t, o_t = 0,0,0,0
+
+
