fdd10f9f895d9d66c0d2421d883bd46d3f88d702
Ulysses B <wagren@kth.se>
2022-05-16 20:45:01 +0200

    nn module refactor, dataloader bugfix in collate_fn

--- leaf/nn/__init__.py
+++ leaf/nn/__init__.py
@@ -1,2 +1,5 @@
-from .nn import Module, Sequential, Linear, LSTM, LogSoftmax, ReLU
+from .nn import Module, Sequential, LSTM, ReLU, Sigmoid, Tanh, LogSoftmax
+from .linear import Linear
+from .conv2d import Conv2d
+from .pool2d import MaxPool2d
 
--- leaf/nn/nn.py
+++ leaf/nn/nn.py
@@ -22,36 +22,22 @@ class Module(object):
 
 class Sequential(object):
     def __init__(self, *modules):
-        self._modules = modules
+        self.modules = modules
 
     def __call__(self, x):
         return self.forward(x)
     
     def forward(self, x):
-        for m in self._modules:
+        for m in self.modules:
             x = m(x)
         return x
     
     def parameters(self):
         params = []
-        for m in self._modules:
+        for m in self.modules:
             params.extend(m.parameters())
         return params
     
-class Linear(Module):
-    def __init__(self, fan_in, fan_out, use_bias=True):
-        self.use_bias = use_bias
-        self.weight_ = Tensor.uniform(fan_in, fan_out, requires_grad=True)
-        self.bias_ = Tensor.uniform(fan_out, requires_grad=True) if use_bias else None
-
-    def forward(self, x):
-        result = x.matmul(self.weight_)
-        
-        if self.use_bias:
-            return result.add(self.bias_)
-
-        return result
-
 class LSTM(Module):
     def __init__(self, input_size, hidden_size, n_layers=1):
         self.input_size = input_size
@@ -99,4 +85,8 @@ class LogSoftmax(Module):
 class ReLU(Module):
     def forward(self, x):
         return x.relu()
+
+class Sigmoid(Module):
+    def forward(self, x):
+        return x.sigmoid()
 
