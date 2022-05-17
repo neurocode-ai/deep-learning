4fca7ec02bd438a5751748ba8d5444676d9787cf
Ulysses B <wagren@kth.se>
2022-05-17 12:37:27 +0200

    load batchnorm2d

--- leaf/nn/__init__.py
+++ leaf/nn/__init__.py
@@ -2,4 +2,4 @@ from .nn import Module, Sequential, LSTM, ReLU, Sigmoid, Tanh, LogSoftmax
 from .linear import Linear
 from .conv2d import Conv2d
 from .pool2d import MaxPool2d
-
+from .batchnorm2d import BatchNorm2d
