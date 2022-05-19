2f683f68a787c9d7290f684e9274ac845fc3b772
Ulysses B <wagren@kth.se>
2022-05-19 08:47:30 +0200

    comply with latest structure changes for nn.Module et al.

--- leaf/nn/__init__.py
+++ leaf/nn/__init__.py
@@ -1,5 +1,8 @@
-from .nn import Module, Sequential, LSTM, ReLU, Sigmoid, Tanh, LogSoftmax
+""" import all of the available neural network Module objects """
+from .nn import Module, Sequential
+from .activations import ReLU, Sigmoid, LogSoftmax, Tanh
 from .linear import Linear
 from .conv2d import Conv2d
 from .pool2d import MaxPool2d
 from .batchnorm2d import BatchNorm2d
+from .lstm import LSTM
